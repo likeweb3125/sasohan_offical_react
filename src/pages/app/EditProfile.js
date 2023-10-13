536fd8955e8ac6992f8a20e7fdb81c01bb1e7de4
sol772 <53934919+sol772@users.noreply.github.com>
2023-10-13 12:24:30 +0900

    페이앱결제 feedbackurl 다시 예전 url 로 돌리기

--- src/pages/app/EditProfile.js
+++ src/pages/app/EditProfile.js
@@ -1,5 +1,21 @@
 const EditProfile = () => {
+    return(<>
+        <div className="edit_profile_wrap">
+            <ul className="top_tab flex_center">
+                <li><a href="#">기본 정보</a></li>
+                <li><a href="#">나의 프로필</a></li>
+                <li><a href="#">이상형 프로필</a></li>
+            </ul>
+            <div className="inner_cont">
+                <div className="line_box">
+                    <p className="top_tit">기본 정보</p>
+                    <div>
 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>);
 };
 
 export default EditProfile;
