4b18fa195344083a016c2f449aa3bf54fee4b334
sol772 <53934919+sol772@users.noreply.github.com>
2025-04-23 13:50:16 +0900

    서비스약관-보증보험증권에 의한 손해보상 청구절차 추가

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -18,150 +18,217 @@ const Footer = (props) => {
     const location = useLocation();
     const [mainPage, setMainPage] = useState(null);
 
-
     //사이트정보 가져오기
     const geInfo = () => {
-        axios.get(`${site_info}`)
-        .then((res)=>{
-            if(res.status === 200){
-                let data = res.data;
-                setInfo({...data});
-            }
-        })
-        .catch((error) => {
-            const err_msg = CF.errorMsgHandler(error);
-            dispatch(confirmPop({
-                confirmPop:true,
-                confirmPopTit:'알림',
-                confirmPopTxt: err_msg,
-                confirmPopBtn:1,
-            }));
-            setConfirm(true);
-        });
+        axios
+            .get(`${site_info}`)
+            .then((res) => {
+                if (res.status === 200) {
+                    let data = res.data;
+                    setInfo({ ...data });
+                }
+            })
+            .catch((error) => {
+                const err_msg = CF.errorMsgHandler(error);
+                dispatch(
+                    confirmPop({
+                        confirmPop: true,
+                        confirmPopTit: "알림",
+                        confirmPopTxt: err_msg,
+                        confirmPopBtn: 1,
+                    })
+                );
+                setConfirm(true);
+            });
     };
 
-    
-    useEffect(()=>{
+    useEffect(() => {
         geInfo();
-    },[]);
-
+    }, []);
 
     //탑버튼
     const scrollToTop = () => {
         window.scroll({
             top: 0,
-            behavior: 'smooth'
-        })
-    }
+            behavior: "smooth",
+        });
+    };
 
     //메인페이지인지 확인
-    useEffect(()=>{
+    useEffect(() => {
         const path = location.pathname;
-        if(path == "/"){
+        if (path == "/") {
             setMainPage(true);
-        }else{
+        } else {
             setMainPage(false);
         }
-    },[location]);
+    }, [location]);
 
-
-    return(<>
-        <footer id="footer" className={mainPage ? "main" : ""}>
-            <div className="top_box">
-                <div className="logo"><img src={logo} alt="로고" /></div>
-                {props.title &&
+    return (
+        <>
+            <footer id="footer" className={mainPage ? "main" : ""}>
+                <div className="top_box">
+                    <div className="logo">
+                        <img src={logo} alt="로고" />
+                    </div>
+                    {props.title && (
+                        <ul>
+                            <li>상호명 : 사소한</li>
+                        </ul>
+                    )}
                     <ul>
-                        <li>상호명 : 사소한</li>
+                        {info && info.site_address && (
+                            <li>주소 : {info.site_address}</li>
+                        )}
+                        {info && info.site_manager && (
+                            <li>
+                                개인정보책임관리자 및 대표 : {info.site_manager}
+                            </li>
+                        )}
                     </ul>
-                }
-                <ul>
-                    {info && info.site_address && <li>주소 : {info.site_address}</li>}
-                    {info && info.site_manager && <li>개인정보책임관리자 및 대표 : {info.site_manager}</li>}
-                </ul>
-                <ul>
-                    {info && info.site_num && <li>사업자 번호 : {info.site_num}</li>}
-                    {info && info.site_tel && <li>연락처 : {info.site_tel}{info.site_fax && ' / '+info.site_fax}</li>}
-                    <li>운영 시간 : 13:00 - 21:30</li>
-                </ul>
-                <ul>
-                    {info && info.site_email && <li>이메일 : {info.site_email}</li>}
-                    <li>본사 연락처 : 070-4355-6649</li>
-                    <li>대표자 : 서정승</li>
-                </ul>
-                <ul>
-                    <li>국내 결혼중개업 신고번호 : 충북-청주-국내-24-0001 호</li>
-                </ul>
-            </div>
-            {props.linkBox &&
-                <div className="link_box">
-                    <ul className="flex_center flex_wrap">
+                    <ul>
+                        {info && info.site_num && (
+                            <li>사업자 번호 : {info.site_num}</li>
+                        )}
+                        {info && info.site_tel && (
+                            <li>
+                                연락처 : {info.site_tel}
+                                {info.site_fax && " / " + info.site_fax}
+                            </li>
+                        )}
+                        <li>운영 시간 : 13:00 - 21:30</li>
+                    </ul>
+                    <ul>
+                        {info && info.site_email && (
+                            <li>이메일 : {info.site_email}</li>
+                        )}
+                        <li>본사 연락처 : 070-4355-6649</li>
+                        <li>대표자 : 서정승</li>
+                    </ul>
+                    <ul>
+                        <li>
+                            국내 결혼중개업 신고번호 : 충북-청주-국내-24-0001 호
+                        </li>
+                    </ul>
+                </div>
+                {props.linkBox && (
+                    <div className="link_box">
+                        <ul className="flex_center flex_wrap">
+                            <li>
+                                <a
+                                    href="/"
+                                    // target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    공지사항
+                                </a>
+                            </li>
+                            <li>
+                                <Link to={"/terms/privacy-policy"}>
+                                    개인정보 보호정책
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to={
+                                        "/terms/personal-information-collection"
+                                    }
+                                >
+                                    개인정보수집
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={"/terms/terms-of-use"}>이용약관</Link>
+                            </li>
+                            <li>
+                                <Link to={"/terms/insurance-claim-procedure"}>
+                                    보증보험증권에 의한 손해보상 청구절차
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                )}
+                <div className="copy">
+                    COPYRIGHT© 2015 사소한 ALL RIGHTS RESERVED.
+                </div>
+            </footer>
+            {props.fixedBox && (
+                <div className="fixed_box">
+                    <ul className="list_sns">
                         <li>
                             <a
-                                href="/"
-                                // target="_blank"
+                                href="https://www.youtube.com/@user-sasohan"
+                                target="_blank"
                                 rel="noopener noreferrer"
-                            >공지사항</a>
+                            >
+                                유튜브
+                            </a>
                         </li>
                         <li>
-                            <Link to={'/terms/privacy-policy'}>개인정보 보호정책</Link>
+                            <a
+                                href="https://www.instagram.com/sasohan1percent_official/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                인스타그램
+                            </a>
                         </li>
                         <li>
-                            <Link to={'/terms/personal-information-collection'}>개인정보수집</Link>
-                        </li>
-                        <li>
-                            <Link to={'/terms/terms-of-use'}>이용약관</Link>
+                            <a
+                                href="https://blog.naver.com/sasohan_official"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                블로그
+                            </a>
                         </li>
                     </ul>
+                    <button
+                        type="button"
+                        className="btn_top"
+                        onClick={scrollToTop}
+                    ></button>
+                    <div className="link_btn_box">
+                        <button
+                            type="button"
+                            className="btn_apply"
+                            onClick={() => {
+                                dispatch(
+                                    aboutVipScroll({
+                                        aboutVipScroll: "vip_sect4",
+                                        aboutVipScrollPath: location.pathname,
+                                    })
+                                );
+                                navigate("/about");
+                            }}
+                        >
+                            소개팅 <br />
+                            신청
+                        </button>
+                        <button
+                            type="button"
+                            className="btn_vip"
+                            onClick={() => {
+                                dispatch(
+                                    aboutVipScroll({
+                                        aboutVipScroll: "vip_sect5",
+                                        aboutVipScrollPath: location.pathname,
+                                    })
+                                );
+                                navigate("/about");
+                            }}
+                        >
+                            1% <br />
+                            지원
+                        </button>
+                    </div>
                 </div>
-            }
-            <div className="copy">COPYRIGHT© 2015 사소한 ALL RIGHTS RESERVED.</div>
-        </footer>
-        {props.fixedBox &&
-            <div className="fixed_box">
-                <ul className="list_sns">
-                    <li>
-                        <a
-                            href="https://www.youtube.com/@user-sasohan"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >유튜브</a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://www.instagram.com/sasohan1percent_official/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >인스타그램</a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://blog.naver.com/sasohan_official"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >블로그</a>
-                    </li>
-                </ul>
-                <button type="button" className="btn_top" onClick={scrollToTop}></button>
-                <div className="link_btn_box">
-                    <button type='button' className='btn_apply'
-                        onClick={()=>{
-                            dispatch(aboutVipScroll({aboutVipScroll:'vip_sect4', aboutVipScrollPath:location.pathname}));
-                            navigate('/about');
-                        }}
-                    >소개팅 <br/>신청</button>
-                    <button type='button' className='btn_vip'
-                        onClick={()=>{
-                            dispatch(aboutVipScroll({aboutVipScroll:'vip_sect5', aboutVipScrollPath:location.pathname}));
-                            navigate('/about');
-                        }}
-                    >1% <br/>지원</button>
-                </div>
-            </div>
-        }
+            )}
 
-        {/* confirm팝업 */}
-        {confirm && <ConfirmPop />}
-    </>);
+            {/* confirm팝업 */}
+            {confirm && <ConfirmPop />}
+        </>
+    );
 };
 
 export default Footer;
--- src/pages/Terms.js
+++ src/pages/Terms.js
@@ -6,82 +6,117 @@ import * as CF from "../config/function";
 import { confirmPop } from "../store/popupSlice";
 import ConfirmPop from "../components/popup/ConfirmPop";
 import { Link, useParams } from "react-router-dom";
-
+import terms_img from "../images/terms_img.jpg";
 
 const Terms = () => {
     const dispatch = useDispatch();
-    const popup = useSelector((state)=>state.popup);
+    const popup = useSelector((state) => state.popup);
     const policy_cont = enum_api_uri.policy_cont;
     const [tabOn, setTabOn] = useState(null);
     const [confirm, setConfirm] = useState(false);
     const [terms, setTerms] = useState({});
     const { terms_tit } = useParams();
 
-
     // Confirm팝업 닫힐때
-    useEffect(()=>{
-        if(popup.confirmPop === false){
+    useEffect(() => {
+        if (popup.confirmPop === false) {
             setConfirm(false);
         }
-    },[popup.confirmPop]);
-
+    }, [popup.confirmPop]);
 
     //약관내용 가져오기
     const getTerms = (idx) => {
-        axios.get(`${policy_cont.replace(":policy_type",idx)}`)
-        .then((res)=>{
-            if(res.status === 200){
-                let data = res.data;
-                setTerms({...data});
-            }
-        })
-        .catch((error) => {
-            const err_msg = CF.errorMsgHandler(error);
-            dispatch(confirmPop({
-                confirmPop:true,
-                confirmPopTit:'알림',
-                confirmPopTxt: err_msg,
-                confirmPopBtn:1,
-            }));
-            setConfirm(true);
-        });
-    }
+        if (idx === 0) return;
+        axios
+            .get(`${policy_cont.replace(":policy_type", idx)}`)
+            .then((res) => {
+                if (res.status === 200) {
+                    let data = res.data;
+                    setTerms({ ...data });
+                }
+            })
+            .catch((error) => {
+                const err_msg = CF.errorMsgHandler(error);
+                dispatch(
+                    confirmPop({
+                        confirmPop: true,
+                        confirmPopTit: "알림",
+                        confirmPopTxt: err_msg,
+                        confirmPopBtn: 1,
+                    })
+                );
+                setConfirm(true);
+            });
+    };
 
-    useEffect(()=>{
+    useEffect(() => {
         let idx;
-        if(terms_tit == 'privacy-policy'){ //개인정보 보호정책
+        if (terms_tit == "privacy-policy") {
+            //개인정보 보호정책
             idx = 1;
-        }else if(terms_tit == 'personal-information-collection'){ //개인정보수집
+        } else if (terms_tit == "personal-information-collection") {
+            //개인정보수집
             idx = 3;
-        }else if(terms_tit == 'terms-of-use'){ //이용약관
+        } else if (terms_tit == "terms-of-use") {
+            //이용약관
             idx = 4;
+        } else if (terms_tit == "insurance-claim-procedure") {
+            //보증보험증권에 의한 손해보상 청구절차
+            idx = 0;
         }
         setTabOn(idx);
         getTerms(idx);
-    },[terms_tit]);
+    }, [terms_tit]);
 
-
-    return(<>
-        <div className="sub_wrap terms_wrap">
-            <div className="top_banner"></div>
-            <div className="inner_cont">
-                <div className="tit_box">
-                    <p className="tit">서비스 약관</p>
-                </div>
-                <div className="round_box">
-                    <ul className="tab_box flex_wrap">
-                        <li className={tabOn === 1 ? 'on' : ''}><Link to={'/terms/privacy-policy'}>개인정보 보호정책</Link></li>
-                        <li className={tabOn === 3 ? 'on' : ''}><Link to={'/terms/personal-information-collection'}>개인정보수집</Link></li>
-                        <li className={tabOn === 4 ? 'on' : ''}><Link to={'/terms/terms-of-use'}>이용약관</Link></li>
-                    </ul>
-                    <div className="txt_box">{terms.contents_p}</div>
+    return (
+        <>
+            <div className="sub_wrap terms_wrap">
+                <div className="top_banner"></div>
+                <div className="inner_cont">
+                    <div className="tit_box">
+                        <p className="tit">서비스 약관</p>
+                    </div>
+                    <div className="round_box">
+                        <ul className="tab_box flex_wrap">
+                            <li className={tabOn === 1 ? "on" : ""}>
+                                <Link to={"/terms/privacy-policy"}>
+                                    개인정보 보호정책
+                                </Link>
+                            </li>
+                            <li className={tabOn === 3 ? "on" : ""}>
+                                <Link
+                                    to={
+                                        "/terms/personal-information-collection"
+                                    }
+                                >
+                                    개인정보수집
+                                </Link>
+                            </li>
+                            <li className={tabOn === 4 ? "on" : ""}>
+                                <Link to={"/terms/terms-of-use"}>이용약관</Link>
+                            </li>
+                            <li className={tabOn === 0 ? "on" : ""}>
+                                <Link to={"/terms/insurance-claim-procedure"}>
+                                    보증보험증권에 의한 손해보상 청구절차
+                                </Link>
+                            </li>
+                        </ul>
+                        {tabOn === 0 ? (
+                            <div className="txt_box">
+                                {`보증보험금 지급\r\n\r\n- 국내결혼중개업자는 결혼중개를 함에 있어서 고의 또는 과실로 인하여 결혼중개업 이용자(이하 이용자 라 함)에게 손해를 발생하게 한 때에는 그 손해를 배상할 책임이 있으며, 손해배상을 하지 아니한 경우에는 보증보험금의 지급을 청구할 수 있습니다.`}
+                                <img src={terms_img} alt="보험금청구절차" />
+                            </div>
+                        ) : (
+                            <div className="txt_box">{terms.contents_p}</div>
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
 
-        {/* confirm팝업 */}
-        {confirm && <ConfirmPop />}
-    </>);
+            {/* confirm팝업 */}
+            {confirm && <ConfirmPop />}
+        </>
+    );
 };
 
 export default Terms;
